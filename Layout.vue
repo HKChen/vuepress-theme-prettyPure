764bcbf177faff9c990ab6471831e92c53fef337
HK Chen <hk_chen@gss.com.tw>
2018-11-26 15:51:50 +0800

    feat: home post list and post item layout.

--- Layout.vue
+++ Layout.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="container">
         <Header />
+        <div class="pp-container">
+            <component class="pp-main-content" :is="layout" />
+        </div>
         <Footer />
     </div>
 </template>
 
 <script>
+import nprogress from 'nprogress'
 import Vue from 'vue'
 import Header from '@theme/components/Header'
+import Home from '@theme/layouts/Home'
+import Post from '@theme/layouts/Post'
 import Footer from '@theme/components/Footer'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
@@ -16,11 +22,38 @@ Vue.config.productionTip = false
 
 export default {
     name: 'Layout',
-
     components: {
         Header,
+        Home,
+        Post,
         Footer
-    }
+    },
+    computed: {
+        layout() {
+            const { path } = this.$page
+            return (path === '/') ? 'home' : 'post'
+        },
+
+        pageClasses() {
+            return `${this.layout}-page`
+        }
+    },
+    mounted() {
+    // progress bar configuration
+    nprogress.configure({ showSpinner: false })
+
+    this.$router.beforeEach((to, from, next) => {
+      if (to.path !== from.path) {
+        nprogress.start()
+      }
+
+      next()
+    })
+
+    this.$router.afterEach(() => {
+      nprogress.done()
+    })
+  }
 }
 </script>
 
