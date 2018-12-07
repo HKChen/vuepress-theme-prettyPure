f862437a8e016194266a70d783040333892a66cc
HK Chen <hk_chen@gss.com.tw>
2018-12-07 10:02:27 +0800

    feat: use fontawesome 5.

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,15 +2,22 @@
     <footer id="pp-footer">
         <p class="pp-footer-text">Copyright © HK Chen
             <a href="https://github.com/hkchen" target="_blank">
-                <i class="fa fa-github-alt" aria-hidden="true"></i>
+                <!-- <i class="fa fa-github-alt" aria-hidden="true"></i> -->
+                <FontAwesome :icon="['fab', 'fa-github-alt']"/>
             </a>
         </p>
     </footer>
 </template>
 
 <script>
+import FontAwesome from '@fortawesome/vue-fontawesome'
+
 export default {
-    name: 'Footer'
+    name: 'Footer',
+
+    components: {
+        FontAwesome
+    },
 }
 </script>
 
--- enhanceApp.js
+++ enhanceApp.js
@@ -1,4 +1,6 @@
 import postMixin from '@theme/mixins/postMixin'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithubAlt }from '@fortawesome/free-brands-svg-icons'
 //import themeMixin from './mixins/themeMixin'
 
 export default ({ Vue, options, router, siteData: { themeConfig } }) => {
