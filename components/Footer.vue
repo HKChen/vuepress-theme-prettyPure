51d8b6f81046a5b5c2a4b30264b22ac2f30eaf45
HK Chen <hk_chen@gss.com.tw>
2018-12-22 11:45:18 +0800

    fix: high severity vulnerability of jQuery pre version.

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <footer id="pp-footer">
         <p class="pp-footer-text">Copyright © HK Chen
             <a href="https://github.com/hkchen" target="_blank">
-                <i class="fa fa-github-alt" aria-hidden="true"></i>
+                <i class="fa fa-github" aria-hidden="true"></i>
             </a>
         </p>
     </footer>
